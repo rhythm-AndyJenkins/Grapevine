c7bfaa1efb3cf7321ebca89402a1c3662368e2a6
Andy Jenkins <andy.jenkins@rhythmagency.com>
2018-02-28 11:36:03 -0800

    Initial commit

---
+++ src/Our.Umbraco.Grapevine/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Our.Umbraco.Grapevine.Application))]
+
+namespace Our.Umbraco.Grapevine {
+    public class Application {
+
+        public void Configuration(IAppBuilder app) {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+    }
+}
---
+++ src/Our.Umbraco.Grapevine/Hubs/UserStatusHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.SignalR;
+
+namespace Our.Umbraco.Grapevine.Hubs {
+
+    public class ChatHub : Hub {
+        public void Send(string name, string message) {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+
+}
